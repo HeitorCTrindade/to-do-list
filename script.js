62c2badb7e89da7b8c8038ae417435223ca26bcf
Heitor C.T <heitor-trindade@hotmail.com>
2022-06-03 11:38:00 -0300

    Requisitos 6,7,8 e 9 implementados.

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ const botaoNTarefa = document.getElementById('criar-tarefa');
 const novaTarefaTexto = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 
-botaoNTarefa.addEventListener('click', function() {
-  console.log('cheguei');
+function mudaCorItemLista() {
+  event.target.classList.add('selecionado');
+  for (let i = 0; i < listaTarefas.childNodes.length; i += 1) {
+    if (listaTarefas.childNodes[i] !== event.target) {
+      listaTarefas.childNodes[i].classList.remove('selecionado');
+      listaTarefas.childNodes[i].classList.add('nao-selecionado');//remover se não for necessario para os proximos requisitos
+    }
+  }
+}
+
+function riscaItemLista() {
+  if (event.target.classList.contains('riscado') === true) {
+    event.target.classList.remove('riscado');
+  } else {
+    event.target.classList.add('riscado');
+  }
+}
+
+botaoNTarefa.addEventListener('click', () => {
   const novaTarefa = document.createElement('li');
   novaTarefa.innerText = novaTarefaTexto.value;
   novaTarefaTexto.value = '';
+  novaTarefa.classList.add('nao-selecionado'); //remover se não for necessario para os proximos requisitos
+  novaTarefa.addEventListener('click', mudaCorItemLista);
+  novaTarefa.addEventListener('dblclick', riscaItemLista);
   listaTarefas.appendChild(novaTarefa);
 });
