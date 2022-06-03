4f077cf4202c9c84ec796a41f4b8a835dbeb707d
Heitor C.T <heitor-trindade@hotmail.com>
2022-06-03 12:25:41 -0300

    Requisitos 10 e 11 finalizados.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const botaoNTarefa = document.getElementById('criar-tarefa');
+const botaoApagaTarefas = document.getElementById('apaga-tudo');
+const botaoApagaTarefasFinalizadas = document.getElementById('remover-finalizados');
 const novaTarefaTexto = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 
@@ -7,16 +9,30 @@ function mudaCorItemLista() {
   for (let i = 0; i < listaTarefas.childNodes.length; i += 1) {
     if (listaTarefas.childNodes[i] !== event.target) {
       listaTarefas.childNodes[i].classList.remove('selecionado');
-      listaTarefas.childNodes[i].classList.add('nao-selecionado');//remover se não for necessario para os proximos requisitos
+      listaTarefas.childNodes[i].classList.add('nao-selecionado'); //remover se não for necessario para os proximos requisitos
     }
   }
 }
 
 function riscaItemLista() {
-  if (event.target.classList.contains('riscado') === true) {
-    event.target.classList.remove('riscado');
+  if (event.target.classList.contains('completed') === true) {
+    event.target.classList.remove('completed');
   } else {
-    event.target.classList.add('riscado');
+    event.target.classList.add('completed');
+  }
+}
+
+function apagaItens (classe) {
+  const tamanhoLista = listaTarefas.children.length;
+  const listaTarefasFinalizadas = document.querySelectorAll(classe);
+  if (classe === undefined) {
+    for (let i = 0; i < tamanhoLista; i += 1) {
+      listaTarefas.removeChild(listaTarefas.children[0]);
+    }
+  } else {
+    for (let i = 0; i < listaTarefasFinalizadas.length; i += 1) {
+      listaTarefas.removeChild(listaTarefasFinalizadas[i]);
+    }
   }
 }
 
@@ -28,4 +44,12 @@ botaoNTarefa.addEventListener('click', () => {
   novaTarefa.addEventListener('click', mudaCorItemLista);
   novaTarefa.addEventListener('dblclick', riscaItemLista);
   listaTarefas.appendChild(novaTarefa);
+});
+
+botaoApagaTarefas.addEventListener('click', () => {
+  apagaItens();
+});
+
+botaoApagaTarefasFinalizadas.addEventListener('click', () => {
+  apagaItens('.completed');
 });
